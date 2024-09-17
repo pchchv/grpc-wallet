1482b0bbbedbda4d9a372a7c6ba708ebe607d8ee
pchchv <ipchchv@gmail.com>
2024-09-17 11:52:11 +0300

    Lint

--- src/client.ts
+++ src/client.ts
@@ -1,20 +1,17 @@
 import { WalletClient, WalletInfoRequest } from "../protos/wallet";
 import { credentials } from "@grpc/grpc-js";
 
-const client = new WalletClient(
-    "localhost:8080",
-    credentials.createInsecure(),
-);
+const client = new WalletClient("localhost:8080", credentials.createInsecure());
 
 const request = new WalletInfoRequest();
-request.setAddress('0x1234567890abcdef');
+request.setAddress("0x1234567890abcdef");
 
 client.walletInfo(request, (error, response) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(response.getTotal());
-        console.log(response.getAvailable());
-        console.log(response.getTransactionsList());
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(response.getTotal());
+    console.log(response.getAvailable());
+    console.log(response.getTransactionsList());
+  }
 });
