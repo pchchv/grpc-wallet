ef44c23e7b30f9f5f878acfc66644933e9a16dd1
pchchv <ipchchv@gmail.com>
2024-09-17 11:48:09 +0300

    Implement a request

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,10 @@
-import { WalletClient } from "../protos/wallet";
+import { WalletClient, WalletInfoRequest } from "../protos/wallet";
 import { credentials } from "@grpc/grpc-js";
 
 const client = new WalletClient(
     "localhost:8080",
     credentials.createInsecure(),
 );
+
+const request = new WalletInfoRequest();
+request.setAddress('0x1234567890abcdef');
